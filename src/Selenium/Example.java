2253c350e8f26252f0281eca43b1ee1c750d9af3
sunny <sunnyj9066@gmail.com>
2017-10-22 19:00:24 +0530

    initial commit

---
+++ src/Selenium/Example.java
@@ -0,0 +1,15 @@
+package Selenium;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=10;i++) {
+			System.out.println("Hello World"+i);
+		}
+		
+		
+
+	}
+
+}
